2bf68024ea8ee56ba1a32f4e344fea85c1aeb6fc
leonardo15043 <lahernandez15043@gmail.com>
2019-02-11 12:20:07 -0500

    Componentes base , enrutamiento de tabs , modelo de datos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+
+import { HomePage , GuardadosPage , TabsPage , MapaPage} from '../pages/index.paginas';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    GuardadosPage,
+    TabsPage,
+    MapaPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +23,10 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    GuardadosPage,
+    TabsPage,
+    MapaPage
   ],
   providers: [
     StatusBar,
